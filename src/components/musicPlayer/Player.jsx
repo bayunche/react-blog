72068d4dea39bc7331c0ee7d398f7a549fdcce0e
bayunche <1035010167@qq.com>
2024-09-03 23:38:54 +0800

    添加模型

--- src/components/musicPlayer/Player.jsx
+++ src/components/musicPlayer/Player.jsx
@@ -4,20 +4,18 @@ const siderLayout = { xxl: 4, xl: 5, lg: 5, sm: 0, xs: 0 }
 
 const Player = () => {
   return (
-    <div>
-      <meting-js
-        server='netease'
-        type='playlist'
-        id='12243826930'
-        fixed='true'
-        autoplay='true'
-        loop='all'
-        order='random'
-        preload='auto'
-        list-folded='ture'
-        lrc-type='3'
-        mutex='true'></meting-js>
-    </div>
+    <meting-js
+      server='netease'
+      type='playlist'
+      id='12243826930'
+      fixed='true'
+      autoplay='true'
+      loop='all'
+      order='random'
+      preload='auto'
+      list-folded='ture'
+      lrc-type='3'
+      mutex='true'></meting-js>
   )
 }
 
--- src/layout/web/index.jsx
+++ src/layout/web/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import '@/styles/app.less'
 import { Layout, Row, Col, BackTop } from 'antd'
-import ReactLive2d from 'react-live2d'
-
+// import ReactLive2d from 'react-live2d'
+import { loadOml2d } from 'oh-my-live2d'
 import Header from './header'
 import SideBar from './sidebar'
 import AppMain from './AppMain'
@@ -15,29 +15,41 @@ const contentLayout = { xxl: 20, xl: 19, lg: 19, sm: 24, xs: 24 }
 
 const Live2d = () => {
   const [canRender, setCanRender] = React.useState(false)
+
   const reg = /home/
   const url = useLocation().pathname
   React.useEffect(() => {
     // console.log(url)
     // 判断是否为首页
-    if (window.innerWidth > 900) {
-      if (reg.test(url)) {
-        setCanRender(true)
-      } else {
-        setCanRender(false)
-      }
+    console.log(reg.test(url))
+
+    if (reg.test(url)) {
+      setCanRender(true)
+      const oml2d = loadOml2d({
+        dockedPosition: 'right',
+        models: [
+          {
+            path: 'https://model.oml2d.com/shizuku_48/index.json',
+
+            scale: 0.2,
+            stageStyle: {
+              height: 450,
+              width: 450,
+            },
+          },
+        ],
+      })
     } else {
       setCanRender(false)
     }
+
+    if (document.getElementById('oml2d-stage') && canRender) {
+      document.getElementById('oml2d-stage').remove()
+    }
   }, [url])
   // 引入live2d模型
-  return (
-    canRender && (
-      <>
-        <ReactLive2d ModelList={['Hiyori', 'miku']} width={400} height={500} />
-      </>
-    )
-  )
+
+  return canRender && <></>
 }
 
 const Beian = () => {
