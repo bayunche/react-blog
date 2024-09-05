30a9330d0c965231ab7ed36afef857279c44562a
bayuncheWorkstation <wangjianjing@spddw.com>
2024-09-05 14:22:32 +0800

    debug

--- server/controllers/article.js
+++ server/controllers/article.js
@@ -378,7 +378,6 @@ class ArticleController {
     const file = ctx.request.files.file // 获取上传文件
 
     await findOrCreateFilePath(uploadPath) // 创建文件目录
-    console.log(file)
     const upload = file => {
       const reader = fs.createReadStream(file.path) // 创建可读流
       const fileName = file.name
