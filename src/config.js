9f66287eaf53c2f40677efcbf62bdf142e4682de
bayuncheWorkstation <wangjianjing@spddw.com>
2024-10-29 16:44:48 +0800

    修改侧边栏子标题以包含达梦数据库信息

--- src/config.js
+++ src/config.js
@@ -18,7 +18,7 @@ export const HEADER_BLOG_NAME = '八云澈的小站' // header title 显示的�
 export const SIDEBAR = {
   avatar: require('@/assets/images/avatar.jpg'), // 侧边栏头像
   title: '八云澈的小站', // 标题
-  subTitle: '痛苦Java中', // 子标题
+  subTitle: '痛苦Java中，抽象的达梦数据库', // 子标题
   // 个人主页
   homepages: {
     github: {
