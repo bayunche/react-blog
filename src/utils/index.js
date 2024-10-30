67c7c6a0371c0c8f3a1f6cce97ff3492580d0fd3
bayuncheWorkstation <wangjianjing@spddw.com>
2024-10-30 08:42:25 +0800

    为代码块添加语言显示和复制功能

--- src/utils/index.js
+++ src/utils/index.js
@@ -75,7 +75,8 @@ export const translateMarkdown2html = plainText => {
     if (lang) {
       lang = lang.toLowerCase()
     }
-    const codeHtml = `<pre><code class="hljs ${lang}">${escaped ? code : xss(code)}</code></pre>`
+    const codeHtml = `<pre><div class="enhance"><div class="lang">${lang}</div><div class="copyCode"><i class="el-icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024"><path fill="currentColor" d="M128 320v576h576V320H128zm-32-64h640a32 32 0 0 1 32 32v640a32 32 0 0 1-32 32H96a32 32 0 0 1-32-32V288a32 32 0 0 1 32-32zM960 96v704a32 32 0 0 1-32 32h-96v-64h64V128H384v64h-64V96a32 32 0 0 1 32-32h576a32 32 0 0 1 32 32zM256 672h320v64H256v-64zm0-192h320v64H256v-64z"></path></svg>
+</i></div><code class="hljs language-${lang}">${escaped ? code : xss(code)}</code></div></pre>`
     return codeHtml
   }
   // 配置marked.js的渲染器为marked_render，使用highlight.js来自动高亮MarkDown中的代码
