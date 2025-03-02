088586dc044e4e4d1b1634c3137082339204a7f7
bayunche <bayunche@gmail.com>
2025-03-02 23:51:34 +0800

    优化Dockerfile和docker-compose.yaml配置

--- mysql_docker/init.sql
+++ mysql_docker/init.sql
@@ -1,8 +1,8 @@
 -- 确保 testuser 存在
 CREATE USER IF NOT EXISTS 'testuser'@'%' IDENTIFIED BY '12345678';
 
--- 赋予 testuser 对 blog 数据库的所有权限
-GRANT ALL PRIVILEGES ON blog.* TO 'testuser'@'%';
+-- 赋予 testuser 对  数据库的所有权限
+GRANT ALL PRIVILEGES ON * TO 'testuser'@'%';
 
 -- 刷新权限，使更改生效
 FLUSH PRIVILEGES;
