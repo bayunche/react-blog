f038e26d3ef5ca06c60ccfb5b8cae050288bd7f4
bayunche <bayunche@gmail.com>
2025-03-02 23:46:05 +0800

    优化安装脚本，移除MySQL用户配置

--- install.sh
+++ install.sh
@@ -16,11 +16,11 @@ nohup yarn serve -s build -l 4356 &
 # 启动后端
 cd ./server && pm2 start app.js --name "blog_server" --watch
 
-# 配置 MySQL 用户
-service mysql start
-mysql -u root -e "CREATE USER 'testuser'@'localhost' IDENTIFIED BY '12345678';"
-mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'testuser'@'localhost';"
-mysql -u root -e "FLUSH PRIVILEGES;"
+# # 配置 MySQL 用户
+# service mysql start
+# mysql -u root -e "CREATE USER 'testuser'@'localhost' IDENTIFIED BY '12345678';"
+# mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'testuser'@'localhost';"
+# mysql -u root -e "FLUSH PRIVILEGES;"
 
 # 导入数据库
 mysql -u testuser -p12345678 < ./server/db/test.sql && echo "数据库导入成功"
