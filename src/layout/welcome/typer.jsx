0945b83aefee49bb9defd8f83711a39ccd1ad74c
bayunche <bayunche@gmail.com>
2025-05-14 19:39:43 +0800

    调整欢迎页面文字内容和样式，修改底部备案信息位置与宽度

--- src/layout/welcome/typer.jsx
+++ src/layout/welcome/typer.jsx
@@ -18,23 +18,12 @@ export class Typer extends React.Component {
     return (
       <div className='typer'>
         <Typist className='typer-header' avgTypingDelay={100} startDelay={2000} onTypingDone={this.onHeaderTyped}>
-          <a href={homepage}>欢迎来到八云澈的小站</a>
+          <a href={homepage}>欢迎来到八云澈的blog</a>
         </Typist>
         <div className='typer-content'>
           {this.state.renderMsg ? (
             <Typist className='typer-message' cursor={{ hideWhenDone: true }}>
-              * 这里记录着技术的点滴进步
-              <Typist.Delay ms={1250} />
-              <br />
-              * 这里记录着生活的五彩斑斓
-              <Typist.Delay ms={1250} />
-              <br />
-              * 希望你也一样，做
-              <Typist.Delay ms={500} />
-              资本的主人
-              <Typist.Backspace count={5} delay={1000} />
-              <Typist.Delay ms={750} />
-              人生的主人
+             和你们这些少爷不同，我们光是活着就竭尽全力了。
               <Typist.Delay ms={1250} />
               <br />
               <span>
